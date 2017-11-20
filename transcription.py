d7254f8e072eaced58fc9af04f89097fbb4f66f1
Karey Shi <verbpronoun@users.noreply.github.com>
2017-11-19 19:30:58 -0800

    sad plotting attempt, need handle dimensions for y_pred

--- transcription.py
+++ transcription.py
@@ -16,7 +16,7 @@ from keras.utils import np_utils
 from keras import backend as K
 from keras.callbacks import Callback
 
-DURATION = 20
+DURATION = 60
 DOWNSAMPLED_SR = 16000
 HOP_LENGTH = 512
 NUM_OCTAVES = 7
@@ -36,10 +36,72 @@ def getFileList():
         midi_files = set([f[:-4] for f in glob.glob(train_dir+'/*.mid')])
         wav_files = set([f[:-4] for f in glob.glob(train_dir+'/*.wav')])
         train_files = midi_files & wav_files
-        print midi_files, wav_files, train_files
+        # print train_files
         for filename in train_files:
             file_list.append((filename+'.wav',filename+'.mid'))
     return file_list
+
+def preprocess_wav_file(filename):
+    # returns 1 example (downsampled, cqt, normalized)
+    y, sr = librosa.load(filename, sr = None, duration=DURATION)
+    y_downsample = librosa.resample(y, orig_sr=sr, target_sr=DOWNSAMPLED_SR)
+    CQT_result = librosa.cqt(y_downsample, sr=DOWNSAMPLED_SR, hop_length=HOP_LENGTH, n_bins=NUM_BINS, bins_per_octave=BINS_PER_OCTAVE)
+    mean = np.mean(CQT_result, axis = 1, keepdims =True)
+    std = np.std(CQT_result, axis = 1, keepdims=True)
+    CQT_result = np.divide(np.subtract(CQT_result, mean), std)
+    
+    paddedX = np.zeros((CQT_result.shape[0], CQT_result.shape[1] + WINDOW_SIZE - 1), dtype=complex)
+    pad_amount = WINDOW_SIZE / 2
+    paddedX[:, pad_amount:-pad_amount] = CQT_result
+    frame_windows = np.asarray([paddedX[:, i:i+WINDOW_SIZE] for i in range(CQT_result.shape[1])])
+    frame_windows = np.expand_dims(frame_windows, axis=3)
+    return frame_windows
+
+def preprocess_midi_truth(filename):
+    # returns 1 ground truth binary vector (size 88)
+    pattern = midi.read_midifile(file(filename))
+    events = NoteEvents(pattern)
+    truth = events.get_ground_truth(31.25, DURATION) # (88, numSlices)
+    return truth
+
+def get_wav_midi_data(filenames):
+    X_list = []
+    Y_list = []
+    for wav_file, midi_file in filenames:
+        X_i = preprocess_wav_file(wav_file)
+        Y_i = preprocess_midi_truth(midi_file)
+        numSlices = min(X_i.shape[0], Y_i.shape[1])
+        X_list.append(X_i[:numSlices])
+        Y_list.append(Y_i[:,:numSlices])
+    X = np.concatenate(X_list, axis = 0)
+    Y = np.concatenate(Y_list, axis = 1)
+    Y = [Y[i] for i in range(Y.shape[0])]
+    return X, Y
+
+def plot_prediction(prediction, target):
+    print prediction.shape
+    print len(target), target[0].shape
+    plt.matshow(prediction)
+    plt.savefig('prediction.jpg')
+    plt.clf()
+    plt.matshow(target)
+    plt.savefig('target.jpg')
+
+class LossHistory(Callback):
+    def on_train_begin(self,logs={}):
+        self.losses =[]
+
+    def on_batch_end(self, batch, logs={}):
+        # print '=====> PRINTING LOGS FOR BATCH [', batch, '] ....'
+        # for key, val in sorted(logs.items()):
+        #     print key, ':', val
+        # print '=====> FINISHED PRINTING LOGS.'
+        self.losses.append(logs.get('loss'))
+
+        # val_predict = np.asarray(self.model.predict(self.model.validation_data[0]))
+        # val_predict = val_predict.round()
+        # val_target = self.model.validation_data[1]
+        # plot_prediction(val_predict[:, :626], [x[:626] for x in val_target])
 
 class Metrics(Callback):
     def on_train_begin(self, logs={}):
@@ -48,7 +110,6 @@ class Metrics(Callback):
         self.val_precisions =[]
 
     def on_epoch_begin(self, epoch, logs={}):
-        print '===================================='
         print 'EPOCH [', epoch, ']:',
 
     def on_epoch_end(self, epoch, logs={}):
@@ -61,6 +122,7 @@ class Metrics(Callback):
         val_predict = val_predict.round()
         val_target = self.model.validation_data[1]
         # print 'PREDICT_SHAPE:', val_predict.shape, '| TARGET_SHAPE:', len(val_target), val_target[0].shape
+        # plot_prediction(val_predict[:, :626, [x[:626] for x in val_target])
         for i in range(val_predict.shape[0]):
             # pred = np.random.uniform(size=(3750,1)).round() 
             # target = np.random.uniform(size=(3750,1)).round()
@@ -91,13 +153,13 @@ def ModelBuilder(input_shape, num_filters, kernel_size_tuples, pool_size, num_hi
     frame_input = Input(shape=input_shape)
     x = Convolution2D(filters=num_filters[0], kernel_size=kernel_size_tuples[0], padding='same', kernel_initializer='he_normal')(frame_input)
     x = Activation('tanh')(x)
-    x = AveragePooling2D(pool_size)(x)
-    x = Dropout(dropout_rate)(x)
+    x = MaxPooling2D(pool_size)(x)
+    # x = Dropout(dropout_rate)(x)
 
     x = Convolution2D(filters=num_filters[1], kernel_size=kernel_size_tuples[1], padding='same', kernel_initializer='he_normal')(x)
     x = Activation('tanh')(x)
-    x = AveragePooling2D(pool_size)(x)    
-    x = Dropout(dropout_rate)(x)
+    x = MaxPooling2D(pool_size)(x)    
+    # x = Dropout(dropout_rate)(x)
 
     x = Flatten()(x)
     x = Dense(num_hidden_units[0], activation='sigmoid')(x)
@@ -112,54 +174,6 @@ def ModelBuilder(input_shape, num_filters, kernel_size_tuples, pool_size, num_hi
         
     return Model(inputs=frame_input, outputs=outputs) 
 
-def preprocess_wav_file(filename):
-    # returns 1 example (downsampled, cqt, normalized)
-    y, sr = librosa.load(filename, sr = None, duration=DURATION)
-    y_downsample = librosa.resample(y, orig_sr=sr, target_sr=DOWNSAMPLED_SR)
-    CQT_result = librosa.cqt(y_downsample, sr=DOWNSAMPLED_SR, hop_length=HOP_LENGTH, n_bins=NUM_BINS, bins_per_octave=BINS_PER_OCTAVE)
-    mean = np.mean(CQT_result, axis = 1, keepdims =True)
-    std = np.std(CQT_result, axis = 1, keepdims=True)
-    CQT_result = np.divide(np.subtract(CQT_result, mean), std)
-    
-    paddedX = np.zeros((CQT_result.shape[0], CQT_result.shape[1] + WINDOW_SIZE - 1), dtype=complex)
-    pad_amount = WINDOW_SIZE / 2
-    paddedX[:, pad_amount:-pad_amount] = CQT_result
-    frame_windows = np.array([paddedX[:, i:i+WINDOW_SIZE] for i in range(CQT_result.shape[1])])
-    frame_windows = np.expand_dims(frame_windows, axis=3)
-    return frame_windows
-
-def preprocess_midi_truth(filename):
-    # returns 1 ground truth binary vector (size 88)
-    pattern = midi.read_midifile(file(filename))
-    events = NoteEvents(pattern)
-    truth = events.get_ground_truth(31.25, DURATION) # (88, numSlices)
-    return truth
-
-def get_midi_ground_truth_data(filenames):
-    for filename in filenames:
-        Y = preprocess_midi_truth(filename)
-        newY = [Y[i] for i in range(Y.shape[0])]
-        print '====== NOTES THAT APPEARED:',
-        for i in range(len(newY)):
-            if 1 in newY[i]:
-                print i, ',',
-        print ''
-        return newY
-
-def get_wav_midi_data(filenames):
-    X_list = []
-    Y_list = []
-    for wav_file, midi_file in filenames:
-        X_i = preprocess_wav_file(wav_file)
-        Y_i = preprocess_midi_truth(midi_file)
-        numSlices = min(X_i.shape[0], Y_i.shape[1])
-        X_list.append(X_i[:numSlices])
-        Y_list.append(Y_i[:,:numSlices])
-    X = np.concatenate(X_list, axis = 0)
-    Y = np.concatenate(Y_list, axis = 1)
-    Y = [Y[i] for i in range(Y.shape[0])]
-    return X, Y
-
 def custom_loss_function(y_true, y_pred):
     print '======== Y_TRUE ========='
     print y_true.data
@@ -173,16 +187,27 @@ def main():
                          kernel_size_tuples=[(25,5), (5,3)], 
                          pool_size=(3,1),
                          num_hidden_units=[200, 200],
-                         dropout_rate=0.5)
-    print model.summary()
-    print '===================================='
+                         dropout_rate=0.1)
+    # model.summary()
+    print '===> Setting up data...'
     X, Y = get_wav_midi_data(getFileList())
-    # print X[0]
+    print '===> Finished setting up data.'
+    print '========================================'
+    lossHistory = LossHistory()
     metrics = Metrics()
     sgd = SGD(lr=LEARNING_RATE, momentum=MOMENTUM_RATE)
-    model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy'])
+    print '===> Compiling the model...'
+    model.compile(optimizer=sgd, loss='hinge', metrics=['accuracy'])
+    print '===> Finished compiling the model.'
+    print '========================================'
     model.validation_data = (X, Y)
-    model.fit(X, Y, validation_data=(X, Y), epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, verbose=0, callbacks=[metrics])
+
+    # EXPERIMENTING WITH PLOTTING
+    val_predict = np.asarray(model.predict(X)).round()
+    val_target = Y
+    plot_prediction(val_predict[:, :626], [x[:626] for x in val_target])
+
+    # model.fit(X, Y, validation_data=(X, Y), epochs=NUM_EPOCHS, batch_size=BATCH_SIZE, verbose=0, callbacks=[lossHistory, metrics])
 
 
 if __name__ == "__main__":
